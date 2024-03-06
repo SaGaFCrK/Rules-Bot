0088d5094b70d9670bfbb2ef3f82508a72a4e797
SaGaFCrK <159914065+SaGaFCrK@users.noreply.github.com>
2024-03-07 02:22:48 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+const { Client, Intents, MessageEmbed, MessageActionRow, MessageSelectMenu } = require('discord.js');
+const rules = require('./rules.json');
+const fs = require('fs');
+const { startServer } = require("./alive.js");
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+
+client.once("ready", () => {
+  console.log(`Bot is Ready! ${client.user.tag}`);
+  console.log(`PROJECT BY PHOENIX TEAM`);
+  console.log(`discord.gg/XVJ6VnD9nM`);
+});
+
+
+client.on('messageCreate', async message => {
+  if (message.content === '!rules') {
+    if (message.member.permissions.has("ADMINISTRATOR")) {
+      const row = new MessageActionRow()
+        .addComponents(
+          new MessageSelectMenu()
+            .setCustomId('select')
+            .setPlaceholder('قائمة القوانين')
+            .addOptions(rules.map(rule => ({
+              label: rule.title,
+              description: rule.id,
+              value: rule.id,
+            }))),
+        );
+
+      const embed = new MessageEmbed()
+        .setColor('#f8ca3d')
+        .setThumbnail('https://cdn.discordapp.com/attachments/1212054605094060064/1215065379534012447/Picsart_24-03-04_12-21-14-401.jpg?ex=65fb64d1&is=65e8efd1&hm=d2ac31b2906e38ee10270a2419b666db51630768a1343a6586d2db2c59f9d994&')
+        .setTitle('قوانين السيرفر')
+        .setDescription('**الرجاء اختيار احد القوانين لقرائته من قائمة الاختيارات تحت**')
+        .setImage('https://cdn.discordapp.com/attachments/1212054605094060064/1215065410974646282/Picsart_24-03-04_12-14-47-434.jpg?ex=65fb64d8&is=65e8efd8&hm=2d85fc0daf7a39b245df67552f404e849938b90bd809459227898174da29036f&')
+        .setFooter({ text: 'Rules Bot' })
+        .setTimestamp();
+
+      const sentMessage = await message.channel.send({ embeds: [embed], components: [row] });
+      await message.delete();
+    } else {
+      await message.reply({ content: "You need to be an administrator to use this command.", ephemeral: true });
+    }
+  }
+});
+
+client.on('interactionCreate', async (interaction) => {
+  if (interaction.isSelectMenu()) {
+    const rule = rules.find(r => r.id === interaction.values[0]);
+    const text = fs.readFileSync(rule.description, 'utf-8');
+    const ruleEmbed = new MessageEmbed()
+      .setColor('#f8ca3d')
+      .setTitle(rule.title)
+      .setDescription(text)
+      .setFooter({ text: 'Rules Bot' })
+      .setTimestamp();
+
+    await interaction.reply({ embeds: [ruleEmbed], ephemeral: true });
+  }
+});
+
+startServer();
+
+client.login(process.env.token);
